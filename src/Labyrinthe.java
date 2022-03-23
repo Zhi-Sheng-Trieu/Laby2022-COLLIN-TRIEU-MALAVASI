7835ff3ceae1581251f18d72e9a3548054e12e5f
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-03-23 15:34:11 +0100

    "Ajout de nouvelles classes et modification de Labyrinthe"

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,164 +1,131 @@
 import java.io.*;
+
 /**
  * Squelette de classe labyrinthe
  */
 public class Labyrinthe {
-    
 
-      public boolean[][] murs;
-      public Personnage personnage;
-      public Sortie sortie;
-      
-      
+    public boolean[][] murs;
+    public Personnage personnage;
+    public Sortie sortie;
 
-      public char getChar(int x, int y) {
-        
-          if ( (y == this.personnage.getY()) && (x == this.personnage.getX()) ) {
-              return ('P');
-          } 
-          
-          else if (this.murs[x][y]) {
-              return ('X');
-          } 
-          
-          else if ((x == this.sortie.getX()) && (y == this.sortie.getY())) {
-              return ('S');
-          } 
-          
-          else {
-              return ('.');
-          }
-    
-      }
+    /*constantes*/
+    public final static char MUR = 'X';
+    public final static char PJ = 'P';
+    public final static char SORTIE = 'S';
+    public final static char VIDE = '.';
+    public final static String HAUT = "haut";
+    public final static String BAS = "bas";
+    public final static String GAUCHE = "gauche";
+    public final static String DROITE = "droite";
 
 
-      public static int[] getSuivant(int x, int y, String direction) {
-        
-          if ( direction == "DROITE" ) {
-            int[] val = new int[2];
-              val[0] = x;
-              val[1] = y+1;
-              return (val);
-          }
-          
-          else if ( direction == "GAUCHE" ) {
-            int[] val = new int[2];
-              val[0] = x-1;
-              val[1] = y;
-              return (val);
-          }
-      
-          else if ( direction == "HAUT" ) {
-            int[] val = new int[2];
-              val[0] = x-1;
-              val[1] = y;
-              return (val);
-          }
+    public char getChar(int x, int y) {
+        char res;
+        if ((y == this.personnage.getY()) && (x == this.personnage.getX())) {
+            res = PJ;
+        } else if (this.murs[x][y]) {
+            res = MUR;
+        } else if ((x == this.sortie.getX()) && (y == this.sortie.getY())) {
+            res = SORTIE;
+        } else {
+            res = VIDE;
+        }
+        return res;
+    }
 
-          else if ( direction == "BAS" ) {
-            int[] val = new int[2];
-              val[0] = x+1;
-              val[1] = y;
-              return (val);
-          }
+    public static int[] getSuivant(int x, int y, String direction) {
+        int[] val = new int[2];
+        if (direction.equals(DROITE)) {
+            val[0] = x;
+            val[1] = y + 1;
+        } else if (direction.equals(GAUCHE)) {
+            val[0] = x - 1;
+            val[1] = y;
+        } else if (direction.equals(HAUT)) {
+            val[0] = x - 1;
+            val[1] = y;
+        } else if (direction.equals(BAS)) {
+            val[0] = x + 1;
+            val[1] = y;
+        } else {
+            val = null;
+        }
 
-          else {
-            return(null);
-          }
-
-      }
+        return val;
+    }
 
 
-      void deplacerPerso(String action) throws ActionInconnueException {
-        
+    void deplacerPerso(String action) {
+
         int y = this.personnage.getY();
         int x = this.personnage.getX();
-        
-        while (getChar(x,y) == 'P' || getChar(x,y) == '.') {
-          
-          int[] val = new int[1];
-            val = getSuivant(x,y,action);
-            
-            if (getChar(x,y) == '.') {
-              x = val[0];
-              y = val[1];
-            }
+        int[] val = getSuivant(x, y, action);
 
+        if (getChar(x, y) == VIDE) {
+            this.personnage.setX(val[0]);
+            this.personnage.setY(val[1]);
         }
-
-        this.personnage.setY(y);
-        this.personnage.setX(x);
-
-      }
+    }
 
 
-      public String toString() {
-      
+    public String toString() {
+
         String tableau = "";
-      
-        for(int i = 0 ; i < this.murs.length ; i++) {
-          
-            for(int i2 = 0 ; i2 < this.murs[i].length ; i2++) {
-              tableau += getChar(i,i2);
+
+        for (int i = 0; i < this.murs.length; i++) {
+
+            for (int j = 0; j < this.murs[i].length; j++) {
+                tableau += getChar(i, j);
             }
 
-        tableau += "\n";
-      
+            tableau += "\n";
+
         }
-      
-        return(tableau);
-    
-      }
+
+        return (tableau);
+    }
 
 
-      public boolean etreFini() {
-          return(this.personnage.getY() == this.sortie.getY() && this.personnage.getX() == this.sortie.getX());
-      }
+    public boolean etreFini() {
+        return (this.personnage.getY() == this.sortie.getY() && this.personnage.getX() == this.sortie.getX());
+    }
 
 
+    public Labyrinthe(String nom) throws IOException {
 
-      public Labyrinthe(String nom){
-        
         BufferedReader fichier = new BufferedReader(new FileReader(nom));
         String ligne = fichier.readLine();
         int x = Integer.parseInt(ligne);
         ligne = fichier.readLine();
         int y = Integer.parseInt(ligne);
-        
-        String caractere;
-        
-        for (int i = 0; i < x; i++) {
-          
-          for (int i2 = 0; i2 < y; i2++) {
-              caractere = fichier.read();
-            
-            if (caractere == 'S') {
-                sortie.setY(i);
-                sortie.setX(i2);
-            }
-              
-            else if (caractere == 'X') {
-              this.murs[i2][i] = true;
-            }
+        this.murs = new boolean[x][y];
 
-            else if (caractere == 'P') {
-                personnage.setY(i);
-                personnage.setX(i2);
+        char caractere;
+        String ln;
+        for (int i = 0; i < x; i++) {
+            ln = fichier.readLine();
+            for (int j = 0; j < y; j++) {
+                if (ln != null) {
+                    caractere = ln.charAt(j);
+                    if (caractere == SORTIE) {
+                        this.sortie = new Sortie(i, j);
+                    } else if (caractere == MUR) {
+                        this.murs[i][j] = true;
+                    } else if (caractere == PJ) {
+                        this.personnage = new Personnage(i, j);
+                    }
+                }
             }
-              
-          }
 
         }
 
-      }
+    }
 
-
-
-      public static Labyrinthe chargerLabyrinthe(String nom) {
-          Labyrinthe lab = new Labyrinthe(nom);
-            return(lab);
-      }
-
+    public static Labyrinthe chargerLabyrinthe(String nom) throws IOException {
+        Labyrinthe lab = new Labyrinthe(nom);
+        return (lab);
+    }
 
 
 }
